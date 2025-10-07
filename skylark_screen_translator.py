28d53fed61abdeb766f53a623883fd7952c21c15
jtliaw <jenntongliaw@yahoo.com.my>
2025-10-07 09:32:18 +0800

    Update skylark_screen_translator.py

--- skylark_screen_translator.py
+++ skylark_screen_translator.py
@@ -5603,7 +5603,7 @@ def main():
         # 禁用Qt的自动DPI缩放
         os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "0"
         os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
-        os.environ["QT_SCALE_FACTOR"] = "1.15"
+        os.environ["QT_SCALE_FACTOR"] = "1.25"
 
     # Linux/macOS 保持高DPI支持
     else:
